4ce11627b97c09717a04cdb4d990fc0de422b08d
Tamás Ádám <atomi2500@gmail.com>
2024-02-06 14:47:44 +0200

    rename fields

--- backend/backend/src/main/java/com/vamk/backend/model/Course.java
+++ backend/backend/src/main/java/com/vamk/backend/model/Course.java
@@ -3,4 +3,4 @@ package com.vamk.backend.model;
 import java.util.UUID;
 
 /* The property "teacher" represents the teacher's name */
-public record Course(UUID uniqueId, String name, String teacher) {}
+public record Course(UUID id, String name, String teacher) {}
--- backend/backend/src/main/java/com/vamk/backend/model/Student.java
+++ backend/backend/src/main/java/com/vamk/backend/model/Student.java
@@ -2,4 +2,4 @@ package com.vamk.backend.model;
 
 import java.util.UUID;
 
-public record Student(UUID uniqueId, String email, String firstName, String lastName) {}
+public record Student(UUID id, String email, String firstName, String lastName) {}
