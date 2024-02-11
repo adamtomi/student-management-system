a8e0d8b0a39cac0bf80f98116ad8fc511c9653b6
Tamás Ádám <atomi2500@gmail.com>
2024-02-12 00:07:30 +0200

    refactor Hash

--- backend/backend/src/main/java/com/vamk/backend/util/Hash.java
+++ backend/backend/src/main/java/com/vamk/backend/util/Hash.java
@@ -5,15 +5,6 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public final class Hash {
-    private static final MessageDigest MESSAGE_DIGEST;
-
-    static {
-        try {
-            MESSAGE_DIGEST = MessageDigest.getInstance("SHA-512");
-        } catch (NoSuchAlgorithmException ex) {
-            throw new ExceptionInInitializerError(ex);
-        }
-    }
 
     private Hash() {}
 
@@ -22,12 +13,17 @@ public final class Hash {
      * of the SHA-512 hash of the provided password.
      */
     public static String sha512(String password) {
-        // Generate hash
-        byte[] bytes = MESSAGE_DIGEST.digest(password.getBytes(StandardCharsets.UTF_8));
-        StringBuilder builder = new StringBuilder();
-        // Convert each byte it's hex representation. Totally not copied from stackoverflow.
-        for (byte each : bytes) builder.append(Integer.toString((each & 0xFF) + 0x100, 16).substring(1));
+        try {
+            // Generate hash
+            byte[] bytes = MessageDigest.getInstance("SHA-512")
+                    .digest(password.getBytes(StandardCharsets.UTF_8));
+            StringBuilder builder = new StringBuilder();
+            // Convert each byte it's hex representation. Totally not copied from stackoverflow.
+            for (byte each : bytes) builder.append(Integer.toString((each & 0xFF) + 0x100, 16).substring(1));
 
-        return builder.toString();
+            return builder.toString();
+        } catch (NoSuchAlgorithmException ex) {
+            throw new RuntimeException(ex);
+        }
     }
 }
