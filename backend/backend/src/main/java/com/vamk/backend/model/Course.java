1b3378573ec714496ce266c8ad50bf7a006d6063
Tamás Ádám <atomi2500@gmail.com>
2024-02-11 22:06:22 +0200

    mark id fields as GeneratedValue

--- backend/backend/src/main/java/com/vamk/backend/model/Course.java
+++ backend/backend/src/main/java/com/vamk/backend/model/Course.java
@@ -2,6 +2,7 @@ package com.vamk.backend.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
@@ -15,6 +16,7 @@ import java.util.UUID;
 @Table(name = "courses")
 public class Course {
     @Id
+    @GeneratedValue
     private UUID id;
     private String name;
     private String teacherName;
--- backend/backend/src/main/java/com/vamk/backend/model/User.java
+++ backend/backend/src/main/java/com/vamk/backend/model/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -14,6 +15,7 @@ import java.util.UUID;
 @Table(name = "users")
 public class User {
     @Id
+    @GeneratedValue
     private UUID id;
     private String firstName;
     private String lastName;
