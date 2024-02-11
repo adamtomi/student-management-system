47a6968daddaf2782b95a9a055e2c2b4d9b30bfc
Tamás Ádám <atomi2500@gmail.com>
2024-02-12 00:27:16 +0200

    finally, authority seems to work

--- backend/backend/src/main/java/com/vamk/backend/config/GlobalConfig.java
+++ backend/backend/src/main/java/com/vamk/backend/config/GlobalConfig.java
@@ -1,20 +1,13 @@
 package com.vamk.backend.config;
 
-import com.vamk.backend.auth.AuthProvider;
 import com.vamk.backend.interceptor.RequestLogger;
 import com.vamk.backend.util.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -34,43 +27,12 @@ public class GlobalConfig implements WebMvcConfigurer {
     }
 
     @Bean
-    UserDetailsService userDetailService() {
-        InMemoryUserDetailsManager userService = new InMemoryUserDetailsManager();
-        UserDetails user = User
-                .withUsername("user")
-                .password(passwordEncoder().encode("password"))
-                .authorities("read")
-                .build();
-        userService.createUser(user);
-        return userService;
-    }
-
-    @Bean
-    BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    /*@Bean
-    AuthenticationProvider authProvider() {
-        return new AuthProvider();
-    }*/
-
-    @Bean
     SecurityFilterChain filterChain(HttpSecurity security) throws Exception {
-        /*return security.authorizeHttpRequests(auth ->
-                /*auth.requestMatchers("/api/course/", "POST")
-                        .hasRole(Role.ADMIN.name())
-                        .requestMatchers("/api/students/", "POST")
-                        .hasRole(Role.ADMIN.name())
-                        .requestMatchers("/api/*")
-                        .hasAnyRole(Role.ADMIN.name(), Role.STUDENT.name())
-                        .anyRequest().authenticated()
-                auth.anyRequest().authenticated()
-        )
-                //.csrf(AbstractHttpConfigurer::disable)
-                //.httpBasic(Customizer.withDefaults())
-                .build();*/
-        return security.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+        return security.authorizeHttpRequests(auth ->
+                auth.requestMatchers("/api/students", "GET").hasAuthority(Role.ADMIN.name()).
+                        anyRequest().authenticated())
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults())
                 .build();
     }
