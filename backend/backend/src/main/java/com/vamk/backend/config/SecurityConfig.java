002dd1661e1601b18800896002d20ed1163d818f
Tamás Ádám <atomi2500@gmail.com>
2024-02-12 00:40:44 +0200

    cleanup

--- backend/backend/src/main/java/com/vamk/backend/config/SecurityConfig.java
+++ backend/backend/src/main/java/com/vamk/backend/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.vamk.backend.config;
 
-import com.vamk.backend.interceptor.RequestLogger;
 import com.vamk.backend.util.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,22 +8,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.List;
-
-@EnableWebMvc
 @Configuration
 @EnableWebSecurity
-public class GlobalConfig implements WebMvcConfigurer {
-
-    // Register interceptors
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new RequestLogger());
-    }
+public class SecurityConfig {
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity security) throws Exception {
