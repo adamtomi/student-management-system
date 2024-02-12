25a69606cd0562008e91da958de72df00ab1c159
Tamás Ádám <atomi2500@gmail.com>
2024-02-12 17:46:12 +0200

    add updateCourse fn

--- frontend/src/api/course.ts
+++ frontend/src/api/course.ts
@@ -1,6 +1,10 @@
-import { doFetch, HttpMethod } from './api.ts'
-import type { Course } from '../types'
+import {doFetch, HttpMethod} from './api.ts'
+import type {Course} from '../types'
 
 export async function getCourses() {
   return doFetch<Course[]>({ endpoint: '/api/courses' })
+}
+
+export async function updateCourse(id: string, data: Partial<Course>) {
+  return doFetch({ endpoint: `/api/courses/${id}`, method: HttpMethod.POST, body: data })
 }
--- frontend/src/components/course/EditCourseModal.tsx
+++ frontend/src/components/course/EditCourseModal.tsx
@@ -2,16 +2,21 @@ import { useState } from 'react'
 import {
   Button,
   Flex,
-  HStack,
+  FormControl,
+  FormLabel,
+  Input,
   Modal,
   ModalBody,
   ModalCloseButton,
   ModalContent,
   ModalFooter,
   ModalHeader,
-  ModalOverlay,
-  Text
+  ModalOverlay
 } from '@chakra-ui/react'
+import { useMutation } from '@tanstack/react-query'
+import { updateCourse } from '../../api/course'
+import { useToast } from '../../hooks/useToast.tsx'
+
 import type { Course } from '../../types'
 
 interface EditCourseModalProps {
@@ -23,18 +28,47 @@ export function EditCourseModal({ close, course }: EditCourseModalProps) {
   const [ courseName, setCourseName ] = useState<string>(course.name)
   const [ teacherName, setTeacherName ] = useState<string>(course.teacherName)
 
+  const toast = useToast()
+
+  const updateCourseMutation = useMutation({
+    mutationKey: [ 'course', course.id, 'update' ],
+    mutationFn: () => updateCourse(course.id, {
+      name: courseName,
+      teacherName
+    }),
+    onError: () => toast.error("Failure", "Failed to update course data."),
+    onSuccess: () => {
+      close()
+      toast.success("Success", "Successfully updated course data.")
+    }
+  })
+
   return (
     <Modal isOpen onClose={close} isCentered>
       <ModalOverlay />
       <ModalContent>
         <ModalHeader>Edit course</ModalHeader>
         <ModalCloseButton />
-        <ModalBody as={Flex} gap={4} direction="column">
 
+        <ModalBody as={Flex} gap={4} direction="column">
+          <FormControl>
+            <FormLabel>Course name</FormLabel>
+            <Input
+              value={courseName}
+              onChange={e => setCourseName(e.target.value)}
+            />
+
+            <FormLabel>Teacher name</FormLabel>
+            <Input
+              value={teacherName}
+              onChange={e => setTeacherName(e.target.value)}
+            />
+          </FormControl>
         </ModalBody>
+
         <ModalFooter gap={4}>
           <Button variant="ghost" onClick={close}>Close</Button>
-          <Button>Submit</Button>
+          <Button onClick={() => updateCourseMutation.mutate()}>Submit</Button>
         </ModalFooter>
       </ModalContent>
     </Modal>
