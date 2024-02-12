549c45d49aad0f7a0e864663d04c9f4800843a4d
Tamás Ádám <atomi2500@gmail.com>
2024-02-12 19:02:37 +0200

    add column annotation

--- backend/backend/src/main/java/com/vamk/backend/model/Course.java
+++ backend/backend/src/main/java/com/vamk/backend/model/Course.java
@@ -1,5 +1,6 @@
 package com.vamk.backend.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,6 +15,7 @@ public class Course {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column(name = "courseName")
     private String name;
     private String teacherName;
 
