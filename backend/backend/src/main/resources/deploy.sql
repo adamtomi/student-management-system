127a7703cfa961551c94fd76d9c56ddcaf9fef1b
Tamás Ádám <atomi2500@gmail.com>
2024-02-13 16:39:18 +0200

    add test course data

--- backend/backend/src/main/resources/deploy.sql
+++ backend/backend/src/main/resources/deploy.sql
@@ -25,3 +25,8 @@ INSERT INTO student_management.users (firstName, lastName, email, password, role
     'c7ad44cbad762a5da0a452f9e854fdc1e0e7a52a38015f23f3eab1d80b931dd472634dfac71cd34ebc35d16ab7fb8a90c81f975113d6c7538dc69dd8de9077ec', -- SHA-512 hash of 'admin'
     0 -- 0 stands for 'ADMIN'
 );
+
+INSERT INTO student_management.courses (courseName, teacherName) VALUES (
+    'Test course 1',
+    'John Doe'
+);
